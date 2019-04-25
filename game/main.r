7ade469c5af4bfb32e1f5d3c66ff3b01de43289a
BrianOtto <brian.otto@zoho.com>
2019-04-24 17:47:01 -0700

    Added levels 2 and 3

--- game/main.r
+++ game/main.r
@@ -1,5 +1,9 @@
-; do read %game.r
-; replace all "" with <b> or links to documentation
+; ?do=game
+; replpad-write as text! read %game/main.r
+; replace all quoted syntax (e.g. "block") with links to documentation
+; print appears to be adding extra line divs sometimes
+
+random/seed now
 
 js-do {
     var beepTimeout = null
@@ -43,6 +47,18 @@ css-do {
         position: relative;
         top: 8px;
     }
+    
+    fieldset {
+        background: #DDD;
+        border: 1px solid #000;
+        padding: 0px 5px;
+        max-width: 400px;
+    }
+    
+    fieldset legend {
+        font-weight: bold;
+        font-family: 'Inconsolata', monospace;
+    }
 }
 
 askUser: function [
@@ -53,14 +69,16 @@ askUser: function [
     /failed
 ][
     if not failed [
-        print request
+        print rejoin [request "^/"]
     ]
     
     prin ">> " resp: input
     
+    trim resp
+    
     either resp == correct [
         do resp
-        print rejoin ["^/^/" success]
+        print rejoin ["^/" success]
     ][
         
         print rejoin ["^/^/" pick failure random length? failure "^/"]            
@@ -101,8 +119,8 @@ begin: function [] [
     
     askUser 
         "backpack: []"
-        "All adventurers need a ^"backpack^" to carry their supplies. Let's create an empty one now using the syntax you just learned.^/"
-        "Excellent, you have created a backpack!"
+        "All adventurers need a backpack to carry their supplies. Let's create an empty one now using the syntax you just learned."
+        "^/Excellent, you have created a backpack!"
         [
             "Nope! Hint: It should be a block so that it can hold multiple items."
             "Nope! Hint: Try naming it ^"backpack^"."
@@ -121,17 +139,65 @@ begin: function [] [
     print "Congratulations! You have been given the power of the all-seeing eye!"
     print "It allows you to probe into the mysterious unknown."
     
-    js-do { beep(999, 100, 1000) }
+    js-do { beep(999, 100, 1500) }
     
-    prin "^/Press any key to continue ..." input
+    prin "^/Press any key to continue ... " input
     
-    print "In Rebol, the ^"probe^" command allows you to see inside variables."
-    print/html "<code>var: ^"123^" probe var</code>"
+    print "^/"
     
-    ; askUser 
-    ;    "probe backpack"
-    ;    "^/This is your backpack. Type probe backpack"
-    ;    "^/There is nothing in your backpack."
+    print "In Rebol, the ^"probe^" command allows you to see the unevaluated ^"value^" that a ^"word^" contains."
+    print "This is useful for debugging, and it can be placed anywhere a value is returned."
+    
+    print "^/Here is an example."
+    
+    print/html "<code>probe pioneers</code>"
+    print/html "<fieldset><legend>Result</legend><pre>[^"John McCarthy^" name ^"Niklaus Wirth^"]</pre></fieldset>"
+    
+    askUser 
+       "probe backpack"
+       "^/Try the ^"probe^" command now, and let's see what is inside your backpack."
+       "Great job, but it looks like there is nothing in your backpack!"
+       ["Hmm, the eye is not working. Maybe you're not looking at it correctly. Try again!"]
+    
+    print "^/Laying on the floor is a few items for your journey."
+    
+    prin "^/Press any key to take a look ... " input
+    
+    prin "^/"
+    
+    print/html "<img src='/game/icons/bread.png' width='100'>"
+    print "'bread"
+    
+    print/html "<img src='/game/icons/rope.png' width='100'>"
+    print "'rope"
+    
+    print/html "<img src='/game/icons/sword.png' width='100'>"
+    print "'sword"
+    
+    print "^/In Rebol, a ^"word^" can be used as a symbol. You do this by prefixing it with a single quote."
+    print "This causes the word to not be evaluated and instead the literal value is returned."
+    
+    askUser 
+       "probe 'bread"
+       "^/Try to ^"probe^" the bread and see what you get."
+       "As you can see, the literal value is returned"
+       ["No, the bread you baffoon. I told you this journey required wits!" "Nope! Hint: Remember to prefix the single quote."]
+    
+    prin "^/Press any key to continue ... " input
+    
+    print/html "<hr>"
+    prin "Level 3"
+    print/html "<hr>"
+    
+    print/html "<img src='/game/icons/map.png' width='100'>"
+    
+    print "Congratulations! You have everything you need to venture outside."
+    print "Let's pack our things and map a route to our next destination."
+    
+    js-do { beep(999, 100, 750) }
+    js-do { beep(999, 500, 750) }
+    
+    prin "^/Press any key to continue ... " input
 ]
 
 begin
